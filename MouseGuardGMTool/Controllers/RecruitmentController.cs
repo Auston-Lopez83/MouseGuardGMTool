852e14e25f05a1e39f9e154d5792a7d21cbfb586
Auston Lopez <Auston Lopez>
2017-06-18 15:23:13 -0500

    Hi

--- MouseGuardGMTool/Controllers/RecruitmentController.cs
+++ MouseGuardGMTool/Controllers/RecruitmentController.cs
@@ -4,13 +4,8 @@ namespace MouseGuardGMTool.Controllers
 {
     public class RecruitmentController : Controller
     {
-<<<<<<< HEAD:MouseGuardGMTool/Controllers/TheGuard.cs
-        private ITheGuardRepository repository;
-        public TheGuard(ITheGuardRepository repo)
-=======
         private IPlayerRepository repository;
         public RecruitmentController(IPlayerRepository repo)
->>>>>>> parent of 7858a07... More updates:MouseGuardGMTool/Controllers/RecruitmentController.cs
         {
             repository = repo;
         }
---
+++ MouseGuardGMTool/Repository/FakePlayerRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MouseGuardGMTool.Models
+{
+    public class       FakePlayerRepository :IPlayerRepository
+    {
+        public IEnumerable<Recruitment> Recruitments => new List<Recruitment>
+        {
+            new Recruitment {Name="Thom", Nature = 4, Will = 4, Health = 4, Resources = 4, Circles = 5},
+            new Recruitment {Name="David", Nature = 4, Will = 4, Health = 4, Resources = 4, Circles = 5},
+            new Recruitment {Name="Bran", Nature = 4, Will = 4, Health = 4, Resources = 4, Circles = 5},
+            new Recruitment {Name="Kyle", Nature = 4, Will = 4, Health = 4, Resources = 4, Circles = 5}
+
+        };
+    }
+}
+
+
+
